d62ae61a8d2bdb8df815c76487d76b8791288da6
Miguel Angel Martinez Pazos <92331020+MPMiguel@users.noreply.github.com>
2023-08-26 17:25:21 +0200

    Add files via upload

---
+++ src/components/TodoForm.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+export const TodoForm = ({addTodo}) => {
+	const[value,setValue] = useState("")
+
+	const handleSubmit = e => {
+		e.preventDefault();
+
+		addTodo(value)
+		
+		setValue("")
+	}
+    return (
+        <form className="TodoForm" onSubmit={handleSubmit}>
+			<input type="text" className="todo-input" value={value} placeholder="What needs to be done?" onChange={(e) => setValue(e.target.value)}></input>
+			<button type="submit" className="todo-btn">Add task</button>
+		</form>
+    )
+}
